8b364bbc76686931559bdf38771339a45cfc9d3b
TaiNK <nguyenkhactai1@gmail.com>
2023-12-08 22:00:13 +0700

    asd

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,5 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(9000);
 }
 bootstrap();
