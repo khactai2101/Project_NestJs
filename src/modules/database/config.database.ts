4f91514b5d4b38c3298dc57410d0d858aff4764d
Tuan Nguyen Duong Anh (T) <tuan.nguyenda@dmchosting.ca>
2023-12-12 14:34:22 +0700

    Change config db

--- src/modules/database/config.database.ts
+++ src/modules/database/config.database.ts
@@ -9,13 +9,13 @@ dotenv.config();
   imports: [
     TypeOrmModule.forRoot({
       type: 'mariadb',
-      host: process.env.DATABASE_HOST || 'localhost',
-      port: +process.env.DATABASE_PORT,
+      host: process.env.DATABASE_HOST,
+      port: 3306,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_DATABASE,
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: false,
+      synchronize: true,
     }),
   ],
 })
